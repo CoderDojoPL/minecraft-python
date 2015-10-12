81570dcc7925f55bbe9e5e2b32ba5b9ed6304717
Kamil Sijko <kamil.sijko@gmail.com>
2015-10-12 14:25:01 +0200

    Update 0_chat.py
    fix wcięć

--- 0_chat.py
+++ 0_chat.py
@@ -3,9 +3,9 @@
 # (np. na RaspberryPi, do którego logujemy się przez SSH)
 import mcpi.minecraft as minecraft
 mc = minecraft.Minecraft.create() 	# jeśli łączymy się z lokalnego Pythona do zdalnego Minecrafta,
-									# to w nawiasie trzeba w cudzysłowiu podać IP serwera Minecraft,
-									# np. ("192.168.0.36"), na RaspberryPi IP sprawdzisz komendą
-									# hostname -I (inne sposoby na https://goo.gl/Vp2qe0)
+					# to w nawiasie trzeba w cudzysłowiu podać IP serwera Minecraft,
+					# np. ("192.168.0.36"), na RaspberryPi IP sprawdzisz komendą
+					# hostname -I (inne sposoby na https://goo.gl/Vp2qe0)
 
 # Wysyłamy testowy komunikat do serwera Minecraft 
 mc.postToChat("Tutaj Kamil - udało mi się! :-)")
